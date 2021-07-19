1db92888ebb2a1ec3b749de3fea493fd29337c0a
Sergej Atamantschuk <sergejatam@gmail.com>
2021-07-19 01:45:15 -0700

    chore: pipeline experiments

--- .github/workflows/buildPipeline.js
+++ .github/workflows/buildPipeline.js
@@ -1,46 +1,58 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
 
-const nodeTemplate = fs.readFileSync("publish-node-service-image.yml", "utf8")
-const pythonTemplate = fs.readFileSync("publish-python-service-image.yml", "utf8")
+const nodeTemplate = fs.readFileSync("publish-node-service-image.yml", "utf8");
+const pythonTemplate = fs.readFileSync(
+    "publish-python-service-image.yml",
+    "utf8"
+);
 
-const coreServices = fs.readdirSync("../../core/services").filter(dir => dir !== "adapter-services").map(dir => ({
-    name: require(path.join("../../core/services", dir, "package.json")).name,
-    path: `core/services/${dir}`,
-    type: "node",
-}))
+const coreServices = fs
+    .readdirSync("../../core/services")
+    .filter((dir) => dir !== "adapter-services")
+    .map((dir) => ({
+        name: require(path.join("../../core/services", dir, "package.json")).name,
+        path: `core/services/${dir}`,
+        type: "node",
+    }));
 
-const adapterServices = fs.readdirSync("../../core/services/adapter-services").map(dir => ({
-    name: require(path.join("../../core/services/adapter-services", dir, "package.json")).name,
-    path: `core/services/adapter-services/${dir}`,
-    type: "node",
-}))
+const adapterServices = fs
+    .readdirSync("../../core/services/adapter-services")
+    .map((dir) => ({
+        name: require(path.join(
+            "../../core/services/adapter-services",
+            dir,
+            "package.json"
+        )).name,
+        path: `core/services/adapter-services/${dir}`,
+        type: "node",
+    }));
 
-const faasNodeServices = fs.readdirSync("../../faas")
+const faasNodeServices = fs
+    .readdirSync("../../faas")
     .map((dir) => ({
         dir,
         contents: fs.readdirSync(path.join("../../faas", dir)),
     }))
-    .filter(({contents}) => contents.includes("package.json"))
-    .map(({dir}) => ({
+    .filter(({ contents }) => contents.includes("package.json"))
+    .map(({ dir }) => ({
         name: require(path.join("../../faas", dir, "package.json")).name,
         path: `faas/${dir}`,
         type: "node",
-    }))
+    }));
 
-const faasPythonServices = fs.readdirSync("../../faas")
+const faasPythonServices = fs
+    .readdirSync("../../faas")
     .map((dir) => ({
         dir,
         contents: fs.readdirSync(path.join("../../faas", dir)),
     }))
-    .filter(({contents}) => contents.includes("setup.py"))
-    .map(({dir}) => {
-        return {
-            name: dir,
-                path: `faas/${dir}`,
-                type: "python",
-        }
-    })
+    .filter(({ contents }) => contents.includes("setup.py"))
+    .map(({ dir }) => ({
+        name: dir,
+        path: `faas/${dir}`,
+        type: "python",
+    }));
 
 const services = [
     ...coreServices,
@@ -49,17 +61,26 @@ const services = [
     {
         name: "web-client",
         path: "core/web-client",
-        type: "node"
+        type: "node",
     },
-]
+];
 
-const buildConfig = (template, service) => fs.writeFileSync(`publish-${service.name}-image.yml`, template.replace(/\+name\+/g, service.name).replace(/\+path\+/g, service.path))
+const buildConfig = (template, service) =>
+    fs.writeFileSync(
+        `publish-${service.name}-image.yml`,
+        template
+            .replace(/\+name\+/g, service.name)
+            .replace(/\+path\+/g, service.path)
+    );
 
-const buildNodeServicePipeline = (service) => buildConfig(nodeTemplate, service)
+const buildNodeServicePipeline = (service) =>
+    buildConfig(nodeTemplate, service);
 
-const buildPythonServicePipeline = (service) => buildConfig(pythonTemplate, service)
+const buildPythonServicePipeline = (service) =>
+    buildConfig(pythonTemplate, service);
 
 for (const service of services) {
-    service.type === "node" ? buildNodeServicePipeline(service) : buildPythonServicePipeline(service)
+    service.type === "node"
+        ? buildNodeServicePipeline(service)
+        : buildPythonServicePipeline(service);
 }
-
