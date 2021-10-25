7ed4b9d8659f9bb2c63d3792c03f871ebecb13cb
Sergej Atamantschuk <sergejatam@gmail.com>
2021-10-25 03:05:17 -0700

    feat(DLA): remove resource objects on delete event

---
+++ core/services/adapter-services/diva-lake-adapter/utils/mongoDbConnectors.js
@@ -0,0 +1,11 @@
+const MongoDBConnector = require("@diva/common/databases/MongoDBConnector");
+
+const dbName = process.env.MONGO_DB_NAME || "divaLakeAdapterDb";
+const collectionName = process.env.MONGO_COLLECTION_NAME || "objects";
+
+const objectsMongoDbConnector = new MongoDBConnector(dbName, [collectionName]);
+
+module.exports = {
+  objectsMongoDbConnector,
+  collectionName,
+};
