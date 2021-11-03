29f24ebac161be63783278367a6497be6ebaee8a
Sergej Atamantschuk <sergejatam@gmail.com>
2021-11-03 20:06:02 +0100

    fix(DA): correctly use database ENV's

--- core/services/adapter-services/dsc-adapter/utils/mongoDbConnectors.js
+++ core/services/adapter-services/dsc-adapter/utils/mongoDbConnectors.js
@@ -3,7 +3,7 @@ const MongoDBConnector = require("@diva/common/databases/MongoDBConnector");
 const resourceDbName = process.env.MONGO_RESOURCE_DB_NAME || "resourcesDb";
 const resourceCollectionName =
   process.env.MONGO_RESOURCE_COLLECTION_NAME || "resources";
-const dscDbName = process.env.MONGO_RESOURCE_DB_NAME || "dscAdapterDb";
+const dscDbName = process.env.MONGO_DSC_DB_NAME || "dscAdapterDb";
 const dscOffersCollectionName =
   process.env.MONGO_DSC_COLLECTION_NAME || "uuidToOfferMappings";
 const dscCatalogsCollectionName =
