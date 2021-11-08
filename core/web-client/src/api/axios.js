91ad94668fd73a026d8554a0e92ce42074642ce4
Sergej Atamantschuk <sergejatam@gmail.com>
2021-11-08 17:23:57 +0100

    fix(client): clean up axios api module

--- core/web-client/src/api/axios.js
+++ core/web-client/src/api/axios.js
@@ -3,19 +3,6 @@ import axios from "axios";
 let endpoint = process.env.VUE_APP_API_GATEWAY_URL || "http://localhost:8000";
 const JWTToken = localStorage.getItem("jwt");
 
-const fetchHeaders = new Headers();
-fetchHeaders.append("Content-Type", "application/json");
-fetchHeaders.append("Authorization", JWTToken ? `Bearer ${JWTToken}` : "");
-fetchHeaders.append("Accept", "application/json");
-
-const customFetch = (url, data = {}) =>
-  fetch(url, {
-    method: "POST",
-    headers: fetchHeaders,
-    mode: "cors",
-    body: JSON.stringify(data),
-  });
-
 export default axios.create({
   baseURL: endpoint,
   headers: {
@@ -24,4 +11,4 @@ export default axios.create({
   },
 });
 
-export { endpoint, customFetch };
+export { endpoint };
