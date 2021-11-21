f99336e17c5295a0955e051851c0a21f414734e2
Daniel Tebernum <daniel.tebernum@gmail.com>
2021-11-21 11:17:20 +0100

    feat(webclient): edit with geojson from backend

--- core/web-client/src/components/Entity/EntityCommonComponents/General/GeneralLocation.vue
+++ core/web-client/src/components/Entity/EntityCommonComponents/General/GeneralLocation.vue
@@ -2,7 +2,6 @@
   <no-data-state v-if="!location" text="You can provide a location here" />
   <l-map v-else ref="map" :zoom="zoom" class="mapstyle">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-geo-json :geojson="location"></l-geo-json>
   </l-map>
 </template>
 
@@ -41,7 +40,12 @@ export default {
 
     this.$nextTick(() => {
       const map = this.$refs.map.mapObject;
-      self.editableLayers = new window.L.FeatureGroup().addTo(map);
+      self.editableLayers = new L.FeatureGroup(self.location).addTo(map);
+
+      const geojsonLayer = L.geoJSON(self.location);
+      geojsonLayer.eachLayer(function (l) {
+        self.editableLayers.addLayer(l);
+      });
 
       this.drawControl = new L.Control.Draw({
         position: "bottomright",
@@ -64,87 +68,12 @@ export default {
         // const type = e.layerType;
         const layer = e.layer;
         console.log(e);
-        console.log(layer);
         // Do whatever else you need to. (save to db, add to map etc)
         self.editableLayers.addLayer(layer);
+        console.log(self.editableLayers.toGeoJSON()); // can be used to be stored in DIVA backend
       });
     });
   },
-  /*
-  mounted() {
-    this.$nextTick(() => {
-      
-      
-
-      window.L.DrawToolbar.include({
-        getModeHandlers(_map) {
-          return [
-            {
-              enabled: true,
-              handler: new window.L.Draw.Polyline(_map),
-              title: "Добавить тоннель",
-            },
-          ];
-        },
-      });
-
-      this.drawControl.setDrawingOptions({
-        // new lines will have different color
-        polyline: {
-          shapeOptions: {
-            color: "#ff0a1e",
-          },
-        },
-      });
-
-      map.addControl(this.drawControl);
-
-      this.editableLayers = new window.L.FeatureGroup().addTo(map);
-      const control = this.drawControl._container.querySelector(
-        ".leaflet-draw-toolbar"
-      );
-
-      let link = document.createElement("a");
-      link.className = "leaflet-draw-draw-OK_ICON";
-      link.style.display = "none";
-      link.onclick = () => {
-        this.$store.dispatch("tunnelEdit", this.newTunnel);
-        this.$emit("tunnelAdd");
-      };
-      this.okBtn = control.appendChild(link);
-
-      link = document.createElement("a");
-      link.className = "leaflet-draw-draw-NOK_ICON";
-      link.style.display = "none";
-      link.onclick = () => {
-        this.okBtn.style.display = "none";
-        this.nokBtn.style.display = "none";
-        this.layers.forEach((l) => this.editableLayers.removeLayer(l));
-        this.newTunnel = {
-          geometry: {
-            coordinates: [],
-          },
-        };
-      };
-      this.nokBtn = control.appendChild(link);
-
-      map.on(window.L.Draw.Event.CREATED, (e) => {
-        const layer = e.layer;
-        const coords = layer._latlngs.map((objCoordinates) => [
-          objCoordinates.lng,
-          objCoordinates.lat,
-        ]);
-
-        this.newTunnel.geometry.coordinates.push(coords);
-        this.newTunnel.categoryDate = 0;
-        this.editableLayers.addLayer(layer);
-        this.layers.push(layer);
-        this.okBtn.style.display = "block";
-        this.nokBtn.style.display = "block";
-      });
-    });
-  },
-  */
 };
 </script>
 
