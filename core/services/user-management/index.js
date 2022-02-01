1d1dbdea8197aef2bcbb0b1e4b883e701349ee6a
Sergej Atamantschuk <sergejatam@gmail.com>
2022-02-01 13:57:58 +0100

    refactor(UM): roll back POC changes

--- core/services/user-management/index.js
+++ core/services/user-management/index.js
@@ -1,11 +1,17 @@
 const createServer = require("@diva/common/api/expressServer");
+const messagesProducer = require("@diva/common/messaging/MessageProducer");
 const jsonSchemaValidator = require("@diva/common/JsonSchemaValidator");
 const usersRouter = require("./routes/users");
 const userImagesRouter = require("./routes/userImages");
 const usersService = require("./services/UsersService");
 const usersImagesService = require("./services/UserImagesService");
+const serviceName = require("./package.json").name;
+
+const NODE_ENV = process.env.NODE_ENV || "development";
+const producer = NODE_ENV === "test" ? () => Promise.resolve() : null;
 
 const port = process.env.PORT || 3001;
+const topic = process.env.KAFKA_EVENT_TOPIC || "user.events";
 const USER_ROOT_SCHEMA = process.env.USER_ROOT_SCHEMA || "user";
 const HISTORY_ROOT_SCHEMA = process.env.HISTORY_ROOT_SCHEMA || "history";
 
@@ -24,6 +30,13 @@ module.exports = createServer(
     app.use("/userImages", userImagesRouter);
 
     await Promise.all([
+      messagesProducer.init(
+        topic,
+        serviceName,
+        "userEvents",
+        "asyncapi",
+        producer
+      ),
       jsonSchemaValidator.init([USER_ROOT_SCHEMA, HISTORY_ROOT_SCHEMA]),
       usersService.init(),
     ]);
