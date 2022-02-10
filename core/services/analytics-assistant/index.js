cf96ae1d0a725330fa580200ff93e10f02d6eb48
Sergej Atamantschuk <sergejatam@gmail.com>
2022-02-10 05:38:08 -0800

    refactor(AA): migrate service

--- core/services/analytics-assistant/index.js
+++ core/services/analytics-assistant/index.js
@@ -1,13 +1,18 @@
-const boot = require("@diva/common/api/expressServer");
+const Server = require("@diva/common/api/expressServer");
 const analyticsRouter = require("./routes/analytics");
 const GlobalAnalyticsService = require("./services/AnalyticsService");
 
 const port = process.env.PORT || 3007;
+const server = new Server(port);
 
-boot(
-  (app) => {
-    app.use("/analytics", analyticsRouter);
-    return GlobalAnalyticsService.init();
-  },
-  { port }
-);
+server.initBasicMiddleware();
+server.addOpenApiValidatorMiddleware();
+server.addMiddleware("/analytics", analyticsRouter);
+
+server
+  .boot()
+  .then(async () => GlobalAnalyticsService.init())
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
