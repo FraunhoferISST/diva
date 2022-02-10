8a83fc591b62bf49f33132a0cc2d33dc6575d38e
Sergej Atamantschuk <sergejatam@gmail.com>
2022-02-10 06:05:06 -0800

    refactor(UPA): migrate service

--- core/services/adapter-services/urban-pulse-adapter/routes/adapter.js
+++ core/services/adapter-services/urban-pulse-adapter/routes/adapter.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const UrbanPulseService = require("../services/UrbanPulseService");
+const urbanPulseService = require("../services/UrbanPulseService");
 
 const router = express.Router();
 
 router.post("/import", async (req, res, next) => {
   try {
     const { createAsset, assetId, streamResponse } = req.query;
-    const result = await UrbanPulseService.import(
+    const result = await urbanPulseService.import(
       req.body,
       req.headers["x-actorid"],
       streamResponse ? { req, res } : {},
@@ -20,18 +20,6 @@ router.post("/import", async (req, res, next) => {
     }
   } catch (e) {
     return next(e?.response?.data || e);
-  }
-});
-
-router.post("/asset", async (req, res, next) => {
-  try {
-    const result = await UrbanPulseService.createAsset(
-      req.body,
-      req.headers["x-actorid"]
-    );
-    res.status(201).send(result);
-  } catch (e) {
-    return next(e);
   }
 });
 
