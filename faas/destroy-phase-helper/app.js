077075a6002fce57f4ae1bea666bad175289e386
Daniel Tebernum <daniel.tebernum@gmail.com>
2022-02-17 16:19:15 +0100

    feat: archive FaaS PoC implemented

--- faas/destroy-phase-helper/app.js
+++ faas/destroy-phase-helper/app.js
@@ -1,88 +1,81 @@
-const { MongoClient, ObjectId } = require("mongodb");
+const { MongoClient } = require("mongodb");
+const { v4: uuidv4 } = require("uuid");
+const axios = require("axios");
+const urljoin = require("url-join");
 
-const mongoURI =
+const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://admin:admin@localhost:27017";
 const DIVA_DB_NAME = process.env.DIVA_DB_NAME || "divaDb";
 const ENTITY_COLLECTION_NAME = process.env.ENTITY_COLLECTION_NAME || "entities";
+const ENTITY_MANAGEMENT_URL =
+  process.env.ENTITY_MANAGEMENT_URL || "http://localhost:3000";
+const serviceId = `faas:uuid:${uuidv4()}`;
 
-class MongoDBConnector {
-  constructor(databaseName = "divaDb", collectionsNames = [], URI = mongoURI) {
-    this.URI = URI;
-    this.databaseName = databaseName;
-    this.collectionsNames = collectionsNames;
+const client = new MongoClient(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 1000 * 60 * 10,
+});
 
-    this.client = null;
-    this.database = null;
-    this.collections = {};
-  }
-
-  async connect() {
-    if (this.client?.topology.isConnected()) {
-      return true;
-    }
-    this.client = new MongoClient(this.URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 1000 * 60 * 10,
-    });
-
-    await this.client.connect();
-    if (this.databaseName) {
-      this.database = this.client.db(this.databaseName);
-      const collections = this.collectionsNames.map((c) => [
-        c,
-        this.database.collection(c),
-      ]);
-      this.collections = Object.fromEntries(collections);
-    }
-    return this.client;
-  }
-
-  async disconnect() {
-    return this.client.close();
-  }
-
-  toObjectId(string) {
-    return ObjectId(string);
-  }
-}
-
-const analyze = async () => {
-  const mongoDbConnector = new MongoDBConnector(DIVA_DB_NAME, [
-    ENTITY_COLLECTION_NAME,
-  ]);
-  await mongoDbConnector.connect();
-  const collection = mongoDbConnector.collections[ENTITY_COLLECTION_NAME];
-  const resultSet = await collection.updateMany(
-    {
-      entityToBeArchivedDate: {
-        $exists: true,
-        $lte: new Date().toISOString(),
-      },
-      $or: [
-        {
-          isArchived: {
-            $exists: false,
-          },
-        },
-        {
-          isArchived: false,
-        },
-      ],
-    },
-    {
-      $unset: {
-        entityToBeArchivedDate: "",
-      },
-      $set: {
+const patchEntity = async (entityId) => {
+  const path = `${entityId.substr(0, entityId.indexOf(":"))}s`;
+  try {
+    await axios.patch(
+      urljoin(ENTITY_MANAGEMENT_URL, path, entityId),
+      {
+        entityToBeArchivedDate: null,
         isArchived: true,
       },
+      {
+        headers: {
+          "x-actorid": serviceId,
+        },
+      }
+    );
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+const analyze = async () => {
+  try {
+    await client.connect();
+    const database = client.db(DIVA_DB_NAME);
+    const entityCollection = database.collection(ENTITY_COLLECTION_NAME);
+
+    const cursor = await entityCollection
+      .find({
+        entityToBeArchivedDate: {
+          $exists: true,
+          $lte: new Date().toISOString(),
+        },
+        $or: [
+          {
+            isArchived: {
+              $exists: false,
+            },
+          },
+          {
+            isArchived: false,
+          },
+        ],
+      })
+      .project({ _id: 0, id: 1 });
+
+    for await (const entity of cursor) {
+      await patchEntity(entity.id);
     }
-  );
-  console.log(resultSet);
-  return true;
+
+    return true;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 analyze()
-  .then(() => console.log("success"))
-  .catch((e) => console.error(e));
+  .then(() => {
+    console.log("success");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
