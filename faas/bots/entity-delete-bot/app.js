1fb68ede82820dbd1c5c09320e2e302aeb1bbeeb
Daniel Tebernum <daniel.tebernum@gmail.com>
2022-02-18 11:36:28 +0100

    feat(faas): implemented archive and delete bot

--- faas/bots/entity-delete-bot/app.js
+++ faas/bots/entity-delete-bot/app.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require("mongodb");
-const { v4: uuidv4 } = require("uuid");
 const axios = require("axios");
 const urljoin = require("url-join");
 
@@ -9,7 +8,7 @@ const DIVA_DB_NAME = process.env.DIVA_DB_NAME || "divaDb";
 const ENTITY_COLLECTION_NAME = process.env.ENTITY_COLLECTION_NAME || "entities";
 const ENTITY_MANAGEMENT_URL =
   process.env.ENTITY_MANAGEMENT_URL || "http://localhost:3000";
-const serviceId = `faas:uuid:${uuidv4()}`;
+const serviceId = "service:uuid:8640d5de-91fe-486a-9378-b8350ede33a5";
 
 const client = new MongoClient(MONGODB_URI, {
   useNewUrlParser: true,
@@ -18,10 +17,13 @@ const client = new MongoClient(MONGODB_URI, {
 });
 
 const patchEntity = async (entityId) => {
-  const path = `${entityId.substr(0, entityId.indexOf(":"))}s`;
   try {
     await axios.patch(
-      urljoin(ENTITY_MANAGEMENT_URL, path, entityId),
+      urljoin(
+        ENTITY_MANAGEMENT_URL,
+        `${entityId.substr(0, entityId.indexOf(":"))}s`,
+        entityId
+      ),
       {
         entityToBeArchivedDate: null,
         isArchived: true,
@@ -38,6 +40,7 @@ const patchEntity = async (entityId) => {
 };
 
 const analyze = async () => {
+  console.log("🤖 Entity Delete Bot: I'm booting...");
   try {
     await client.connect();
     const database = client.db(DIVA_DB_NAME);
@@ -45,37 +48,32 @@ const analyze = async () => {
 
     const cursor = await entityCollection
       .find({
-        entityToBeArchivedDate: {
+        entityToBeDeletedDate: {
           $exists: true,
           $lte: new Date().toISOString(),
         },
-        $or: [
-          {
-            isArchived: {
-              $exists: false,
-            },
-          },
-          {
-            isArchived: false,
-          },
-        ],
       })
       .project({ _id: 0, id: 1 });
 
     for await (const entity of cursor) {
       await patchEntity(entity.id);
+      console.log(
+        `🤖 Entity Delete Bot: I deleted entity with id: ${entity.id}`
+      );
     }
 
     return true;
   } catch (err) {
     throw new Error(err);
+  } finally {
+    client.close();
   }
 };
 
 analyze()
   .then(() => {
-    console.log("success");
+    console.log("🤖 Entity Delete Bot: I finished successfully!");
   })
-  .catch((e) => {
-    console.error(e);
+  .catch((err) => {
+    console.error(err);
   });
