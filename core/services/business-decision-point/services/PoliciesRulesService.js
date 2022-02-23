04357369f35999fdb6761d36846a08eaef887361
Janis Büse <janis.buese@isst.fraunhofer.de>
2022-02-23 16:26:54 +0100

    chore(policies): added boilerplate code for evaluation

--- core/services/business-decision-point/services/PoliciesRulesService.js
+++ core/services/business-decision-point/services/PoliciesRulesService.js
@@ -1,4 +1,8 @@
-const policies = require("../static/rules.json");
+const _ = require("lodash");
+const { mongoDBConnector, neo4jConnector } = require("../utils/dbConnectors");
+const { isConditionMet } = require("../utils/utils");
+
+const policies = require("../static/policyRules");
 
 class PoliciesRulesService {
   constructor() {
@@ -6,13 +10,41 @@ class PoliciesRulesService {
   }
 
   async init() {
+    await mongoDBConnector.connect();
+    await neo4jConnector.connect();
+    this.neo4jClient = neo4jConnector.client;
+
     // TODO: load policies from db
-    return true;
+    // return true;
   }
 
   async enforcePolicies(req) {
-    // TODO: do some stuff
-    return true; // or false;
+    this.actorid = req.body["x-actorid"];
+    this.entityid = req.body.entityid;
+
+    const constraints = [];
+    let resolvedConditions = [];
+    // TODO GET policies
+
+    await Promise.all(
+      policies.map(async (policy) => {
+        resolvedConditions = await isConditionMet(policy.condition, {
+          entityid: this.entityid,
+          actorid: this.actorid,
+        });
+      })
+    );
+
+    console.log("final result", resolvedConditions);
+    return {
+      decision: resolvedConditions,
+      constraints: this.merge(constraints),
+    };
+
+  }
+
+  merge(constraints) {
+    return constraints;
   }
 }
 
---
+++ core/services/business-decision-point/static/policyRules.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    title: "Creator of resource can perform GET",
+    priority: 1,
+    methods: ["GET"],
+    scope: ["resource-management::resources/*"],
+    condition: {
+      and: [
+        {
+          cypher: {
+            query:
+              "MATCH (r:resource {entityId:'{{entityid}}'})<-[e:isCreatorOf]-(:user {entityId:'{{actorid}}'}) RETURN (count(e)>0) as ruleMet",
+          },
+        },
+      ],
+    },
+    actions: [],
+    constraints: ["E", "!F"],
+  },
+  {
+    title: "Owner of resource can perform GET",
+    priority: 1,
+    methods: ["GET"],
+    scope: ["resource-management::resources/*"],
+    condition: {
+      and: [
+        {
+          cypher: {
+            query:
+              "MATCH (r:resource {entityId:'{{entityid}}'})<-[e:isOwnerOf]-(:user {entityId:'{{actorid}}'}) RETURN (count(e)>0) as ruleMet",
+          },
+        },
+      ],
+    },
+    actions: [],
+    constraints: ["A", "!B"],
+  },
+  {
+    title: "User can GET resource if both entities are part of asset",
+    priority: 1,
+    methods: ["GET"],
+    scope: ["resource-management::resources/*"],
+    condition: {
+      and: [
+        {
+          cypher: {
+            query:
+              "MATCH (r:resource {entityId:'{{entityid}}'})-[:isPartOf]->(a:asset)<-[:isPartOf]-(:user {entityId:'{{actorid}}'}) RETURN (count(a)>0) as ruleMet",
+          },
+        },
+        {
+          cypher: {
+            query:
+              "MATCH (r:resource {entityId:'{{entityid}}'})-[:isPartOf]->(a:asset)<-[:isPartOf]-(:user {entityId:'{{actorid}}'}) RETURN (count(a)>0) as ruleMet",
+          },
+        },
+      ],
+    },
+    actions: [],
+    constraints: ["C", "!D"],
+  },
+];
