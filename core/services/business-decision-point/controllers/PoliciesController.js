496db5d0ba65333fc95d7c043c4ca9c3af4aea76
Daniel Tebernum <daniel.tebernum@gmail.com>
2022-04-23 08:03:19 +0200

    chore(bdp): removed comment

--- core/services/business-decision-point/controllers/PoliciesController.js
+++ core/services/business-decision-point/controllers/PoliciesController.js
@@ -4,7 +4,6 @@ class PoliciesController {
   async enforcePolicies(req, res, next) {
     try {
       const result = await policiesService.enforcePolicies(req);
-      // TODO: response with relevant data
       res.status(200).send(result);
     } catch (err) {
       return next(err);
