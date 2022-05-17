c27eafea766c0c3e3d7e59de2aa2cba3baf4fbae
Sergej Atamantschuk <sergejatam@gmail.com>
2022-05-17 11:39:48 +0200

    fix(BDP): correctly set services URL's mapping from ENV's

--- core/services/business-decision-point/utils/servicesURLs.js
+++ core/services/business-decision-point/utils/servicesURLs.js
@@ -1,7 +1,8 @@
 const SERVICES_URLS =
-  process.env.SERVICES_URLS || process.env.DIVA_HOST
+  process.env.SERVICES_URLS ||
+  (process.env.DIVA_HOST
     ? `entity-management::http://${process.env.DIVA_HOST}:3000,profiling-assistant::http://${process.env.DIVA_HOST}:3011`
-    : "entity-management::http://localhost:3000,profiling-assistant::http://localhost:3011";
+    : "entity-management::http://localhost:3000,profiling-assistant::http://localhost:3011");
 
 const servicesURLsMap = Object.fromEntries(
   SERVICES_URLS.split(",").map((dependencyGroup) => dependencyGroup.split("::"))
