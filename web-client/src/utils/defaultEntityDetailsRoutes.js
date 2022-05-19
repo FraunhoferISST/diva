b93ef9cf806a14209d846462e1a68dbf64336b73
Sergej Atamantschuk <sergejatam@gmail.com>
2022-05-19 11:37:06 +0200

    feat(client): improve routes generation

---
+++ web-client/src/utils/defaultEntityDetailsRoutes.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: "Overview",
+    icon: "short_text",
+    name: "entity_details_general",
+  },
+  {
+    title: "History",
+    icon: "history",
+    name: "entity_details_history",
+  },
+  {
+    title: "Reviews",
+    icon: "question_answer",
+    name: "entity_details_reviews",
+  },
+  {
+    title: "Data Network",
+    icon: "insights",
+    name: "entity_details_datanetwork",
+  },
+];
