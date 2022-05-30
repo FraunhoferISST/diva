dbc96c084bb2b86b9fb26149c9b32c392a884872
Daniel Tebernum <daniel.tebernum@gmail.com>
2022-05-30 15:08:26 +0200

    feat: added folders and destroyclaim basics to dev

--- web-client/src/api/entities.js
+++ web-client/src/api/entities.js
@@ -1,5 +1,13 @@
 import apiFactory from "@/api/apiFactory";
-const entityTypes = ["resource", "asset", "user", "review", "service"];
+const entityTypes = [
+  "resource",
+  "asset",
+  "user",
+  "review",
+  "service",
+  "folder",
+  "destroyclaim",
+];
 export default {
   ...Object.fromEntries(
     entityTypes.map((type) => [`${type}s`, apiFactory(`/${type}s`)])
