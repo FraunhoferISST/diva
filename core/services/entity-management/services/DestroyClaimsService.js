dd0b22cb21590c0c95411ab05027952550a37a0b
Daniel Tebernum <daniel.tebernum@gmail.com>
2023-01-13 11:59:40 +0100

    feat: implemented resolving of existing fields

--- core/services/entity-management/services/DestroyClaimsService.js
+++ core/services/entity-management/services/DestroyClaimsService.js
@@ -1,20 +1,70 @@
 const { entityNotFoundError } = require("@diva/common/Error");
+const DataNetworkService = require("./DataNetworkService");
+
 const EntityService = require("./EntityService");
 const {
   entityTypes: { DESTROY_CLAIM },
 } = require("../utils/constants");
 
-const resolveDestroyClaim = (destroyclaim) => ({
-  id: destroyclaim.id.split(":").pop(),
-  isActive: destroyclaim.isActive,
-  modelVersion: destroyclaim.modelVersion,
-  issued: destroyclaim.createdAt,
-  modified: destroyclaim.modifiedAt,
-});
+const shortenDivaId = (divaId) => {
+    return divaId.split(":").pop();
+}
 
 class DestroyClaimService extends EntityService {
   async init() {
     await super.init();
+  }
+
+  async #resolveDestroyContacts(destroyclaim) {
+    const destroyclaimOwners = await DataNetworkService.getEdges({
+      edgeTypes: ["isOwnerOf"],
+      to: destroyclaim.id,
+      fromNodeType: "user",
+      toNodeType: "destroyclaim",
+    });
+
+    const contacts = await Promise.all(destroyclaimOwners.collection.map(async (u) => {
+      const entity = await this.collection.findOne(
+        {
+          id: u.from.entityId,
+        },
+        { projection: { _id: false } }
+      );
+      if (entity) {
+        return {
+          id: shortenDivaId(entity.id),
+          name: "std:agent",
+          payload: {
+            name: entity.username,
+            mbox: entity.email,
+          },
+          refs: [shortenDivaId(destroyclaim.id)],
+        };
+      }
+      throw entityNotFoundError;
+    }));
+
+    return contacts;
+  }
+
+  async #resolveDestroyClaim(destroyclaim) {
+    return {
+      id: shortenDivaId(destroyclaim.id),
+      isActive: destroyclaim.isActive,
+      modelVersion: destroyclaim.modelVersion,
+      expirationDate: destroyclaim.destroyclaimExpirationDate,
+      strictMode: destroyclaim.strictMode,
+      simulationMode: destroyclaim.simulationMode,
+      notificationMode: destroyclaim.notificationMode,
+      manualMode: destroyclaim.manualMode,
+      issued: destroyclaim.createdAt,
+      modified: destroyclaim.modifiedAt,
+      title: destroyclaim.title,
+      description: destroyclaim.description,
+      keywords: destroyclaim.keywords,
+      destroyReasons: destroyclaim.destroyclaimDestroyReasons?.map((r) => r.value),
+      destroyContacts: await this.#resolveDestroyContacts(destroyclaim),
+    };
   }
 
   async getAndResolveById(id) {
@@ -25,7 +75,7 @@ class DestroyClaimService extends EntityService {
       { projection: { _id: false } }
     );
     if (entity) {
-      return resolveDestroyClaim(entity);
+      return this.#resolveDestroyClaim(entity);
     }
     throw entityNotFoundError;
   }
