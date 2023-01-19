974a0e8de20e2a14e345c87e05081d21daceae58
Daniel Tebernum <daniel.tebernum@gmail.com>
2023-01-19 11:19:39 +0100

    fix(client): typo

--- web-client/src/views/NotFound.vue
+++ web-client/src/views/NotFound.vue
@@ -12,7 +12,7 @@
           class="fill-height d-flex justify-center align-center"
         >
           <div class="text-center">
-            <no-data-state> The requested page were not found </no-data-state>
+            <no-data-state> The requested page was not found </no-data-state>
             <p>
               Go back or
               <router-link to="/"> start again</router-link>
