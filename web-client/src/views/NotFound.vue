cceab22da498c4138b3c758f356020e75aff0421
Daniel Tebernum <daniel.tebernum@gmail.com>
2023-01-19 11:13:22 +0100

    feat(cleint): improved 404

--- web-client/src/views/NotFound.vue
+++ web-client/src/views/NotFound.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="not-found" class="fill-height d-flex justify-center align-center">
-    <div class="text-center">
-      <no-data-state> The requested page were not found </no-data-state>
-      <p>
-        Go back or
-        <router-link to="/"> start again</router-link>
-      </p>
-    </div>
-  </div>
+  <v-container fill-height>
+    <v-row align="center">
+      <v-col cols="12" sm="4" offset-sm="4">
+        <v-img
+          src="@/assets/illustrations/undraw_page_not_found_re_e9o6.svg"
+        ></v-img>
+      </v-col>
+      <v-col cols="12" sm="4" offset-sm="4">
+        <div
+          id="not-found"
+          class="fill-height d-flex justify-center align-center"
+        >
+          <div class="text-center">
+            <no-data-state> The requested page were not found </no-data-state>
+            <p>
+              Go back or
+              <router-link to="/"> start again</router-link>
+            </p>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
